64e4704d70ffc6a706d88f17d737f79f5059d600
BrunoCry <danil-az2@mail.ru>
2024-05-12 18:44:43 +0300

    Create LoansController

---
+++ app/Http/Controllers/API/LoansController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+
+class LoansController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+}
