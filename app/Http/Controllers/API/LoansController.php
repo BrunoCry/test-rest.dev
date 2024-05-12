94edfde7806d4f8d037cd7a4b5df912a68dc32d1
BrunoCry <danil-az2@mail.ru>
2024-05-12 19:08:04 +0300

    Return of all models

--- app/Http/Controllers/API/LoansController.php
+++ app/Http/Controllers/API/LoansController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Loan;
 
 class LoansController extends Controller
 {
     public function index()
     {
-        //
+        return response()->json([
+            'data' => Loan::all()
+        ]);
     }
 }
